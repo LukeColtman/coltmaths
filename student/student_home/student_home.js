03335879227099a9d8e9af2c6f437a4d78643dd1
LinkyLuke <25992603+LinkyLuke@users.noreply.github.com>
2019-07-25 14:55:16 +0100

    js functions for dropdown

--- student/student_home/student_home.js
+++ student/student_home/student_home.js
@@ -2,12 +2,78 @@ function myFunction() {
     document.getElementById("dropdown_number").classList.toggle("show");
 }
 
+// Number click functions
+
+function arithmeticClick() {
+    document.getElementById("number_arithmetic").classList.toggle("show");
+}
+
+function arithmeticClick() {
+    document.getElementById("number_arithmetic").classList.toggle("show");
+}
+
+function bidmasClick() {
+    document.getElementById("number_bidmas").classList.toggle("show");
+}
+
+function decimalClick() {
+    document.getElementById("decimal_bidmas").classList.toggle("show");
+}
+
+function fractionsClick() {
+    document.getElementById("number_fractions").classList.toggle("show");
+}
+
+function indicesClick() {
+    document.getElementById("number_indices").classList.toggle("show");
+}
+
+function fdpClick() {
+    document.getElementById("number_fdp").classList.toggle("show");
+}
+
+function negativesClick() {
+    document.getElementById("number_negatives").classList.toggle("show");
+}
+
+function placeValueClick() {
+    document.getElementById("number_placeValue").classList.toggle("show");
+}
+
+function percentagesClick() {
+    document.getElementById("number_percentages").classList.toggle("show");
+}
+
+function powersOfTenClick() {
+    document.getElementById("number_powersOfTen").classList.toggle("show");
+}
+
+function numberPropertiesClick() {
+    document.getElementById("number_numberProperties").classList.toggle("show");
+}
+
+function recurringDecimalsClick() {
+    document.getElementById("number_recurringDecimals").classList.toggle("show");
+}
+
+function standardFormClick() {
+    document.getElementById("number_standardForm").classList.toggle("show");
+}
+
 function surdClick() {
     document.getElementById("number_surds").classList.toggle("show");
 }
 
-function loiClick() {
-    document.getElementById("number_loi").classList.toggle("show");
+function timetablesClick() {
+    document.getElementById("number_timetables").classList.toggle("show");
+}
+
+function boundsClick() {
+    document.getElementById("number_bounds").classList.toggle("show");
+}
+
+function usingCalculatorClick() {
+    document.getElementById("number_usingCalculator").classList.toggle("show");
 }
 
 window.onclick = function(event) {
